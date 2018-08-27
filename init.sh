64ed59ad6f7da20fa1237ebf8225985e05df8e6f
Amund Tenstad <github@amund.io>
2018-08-27 07:27:24 +0000

    Fix typo

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx docker.io
-sudo mv nginx.conf /etc/nginx/sites-enables/default
+sudo mv nginx.conf /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
 sudo docker build . -t jupyter
