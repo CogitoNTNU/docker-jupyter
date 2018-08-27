1447ce07ce27f52f9b2591b6385970586ac9b21a
Amund Tenstad <github@amund.io>
2018-08-27 07:53:11 +0000

    Create launch script

---
+++ launch.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import uuid
+
+def launch(n):
+	for i in range(1, int(n)+1):
+		password = uuid.uuid4().hex[:6]
+		group = str(i).rjust(2, '0')
+		os.system('sudo docker run -d -p 80%s:8888 -e BASE_URL=%s -e PASSWORD=%s jupyter' % (group, group, password))
+		message = 'Group %s: %s' % (group, password)
+		print(message)
+		with open('password.txt', 'a') as f:
+			f.write(message + '\n')
+
+launch(sys.argv[1])
