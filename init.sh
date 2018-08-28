dab423d4104691d0ab1b8749f1de833e91290eb3
Amund Tenstad <github@amund.io>
2018-08-28 07:52:18 +0000

    Add zip command

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install nginx docker.io
+sudo apt-get -y install nginx docker.io zip
 sudo mv nginx.conf /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
 sudo docker build . -t jupyter
---
+++ zip.sh
@@ -0,0 +1,1 @@
+zip -r notebooks_$(date +%Y-%m-%d_%H-%M-%S).zip notebooks
