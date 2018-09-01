ca72032cc45b85e91fea29a4cd41ffdd97d3b913
Amund Tenstad <github@amund.io>
2018-09-01 11:44:16 +0200

    Simplify jupyter_conf.py

--- container/setup/jupyter_conf.py
+++ container/setup/jupyter_conf.py
@@ -1,19 +1,13 @@
-import sys
-import hashlib
-import uuid
-import os
-
-base_url = sys.argv[2] if len(sys.argv) > 2 else ''
-salt = uuid.uuid4().hex
-password = hashlib.sha256((sys.argv[1] + salt).encode()).hexdigest()
+import sys, hashlib, uuid, os
 
 with open('/code/tmp/jupyter.conf', 'r') as f:
 	content = f.read()
 
+salt = uuid.uuid4().hex
 content = content % {
 	'salt': salt,
-	'password': password,
-	'base_url': base_url,
+	'password': hashlib.sha256((sys.argv[1] + salt).encode()).hexdigest(),
+	'base_url': sys.argv[2] if len(sys.argv) > 2 else '',
 }
 
 with open('/root/.jupyter/jupyter_notebook_config.py', 'w') as f:
